ad6671a3354f34de4a9bef582c5d6857ebb9d838
Rhenn3D <aalvarez@ctsformacion.com>
2025-04-26 01:09:21 +0200

    Todo acabado: Movimientos+Salto+Animaciones+Mov. Cam y fondo+Escenario

--- Assets/Scripts/Oskar.cs
+++ Assets/Scripts/Oskar.cs
@@ -11,11 +11,13 @@ public class Oskar : MonoBehaviour
     public float jumpForce = 8f;
 
     public GroundSensor groundSensor;
-    private float inputHorizontal;
+    public float inputHorizontal;
+    private Animator _animator;
     void Awake()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
         groundSensor = GetComponentInChildren<GroundSensor>();
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -23,17 +25,25 @@ public class Oskar : MonoBehaviour
         if(inputHorizontal > 0)
             {
 	        transform.rotation = Quaternion.Euler(0, 0, 0);
+            _animator.SetBool("IsRunning", true);
             }
         else if(inputHorizontal < 0)
             {
 	        transform.rotation = Quaternion.Euler(0, 180, 0);
+            _animator.SetBool("IsRunning", true);
             }
+            else
+                _animator.SetBool("IsRunning", false);
+
 
         inputHorizontal = Input.GetAxisRaw("Horizontal");
-        if(Input.GetButtonDown("Jump") && groundSensor.isGrounded)
+        if(Input.GetButtonDown("Jump") && groundSensor.isGrounded == true)
         {
             _rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            _animator.SetBool("IsRunning", false);
+            _animator.SetBool("IsJumping", true);
         }
+        _animator.SetBool("IsJumping", !groundSensor.isGrounded);
         
         
     }
@@ -43,7 +53,7 @@ public class Oskar : MonoBehaviour
         _rigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * playerSpeed, _rigidBody.velocity.y);
 
     }
-   
+
     
 
 
